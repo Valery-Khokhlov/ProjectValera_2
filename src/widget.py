e02b1901712d4b3082b30e13c26f7e9e1121a8e0
Valery Khokhlov <xoxlov_v83@mail.ru>
2026-02-23 19:06:03 +0300

    widget creation

---
+++ src/widget.py
@@ -0,0 +1,27 @@
+
+import re
+
+from ..src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(account_card: str | int) -> str:
+
+    """ Функция принимает строку, содержащую тип и номер карты или счета
+        и возвращает строку с замаскированным номером """
+
+    if re.match(r"Счет", account_card):
+        mask_account = "".join(re.findall(r"\d", account_card))
+        return re.sub(r"\d{20}", get_mask_account(mask_account), account_card)
+    else:
+        mask_card = "".join(re.findall(r"\d", account_card))
+        return re.sub(r"\d{16}", get_mask_card_number(mask_card), account_card)
+
+
+def get_date(original_date: str | int ) -> str:
+
+    """ Функция принимает на вход строку с датой в формате
+        "2024-03-11T02:26:18.671407" и возвращает строку с датой в формате
+        "ДД.ММ.ГГГГ" ("11.03.2024"). """
+
+    match = re.search(r"(\d{4})-(\d{2})-(\d{2})", original_date)
+    return str(f"{match.group(3)}.{match.group(2)}.{match.group(1)}")
