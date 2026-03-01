1f5b2b4db1694487076f9d1adda78a225cbbf481
Valery Khokhlov <xoxlov_v83@mail.ru>
2026-03-01 21:35:44 +0300

    processing change

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,19 @@
-def filter_by_state(list_state: list[dict], state: str = "EXECUTED") -> list[dict]:
+from typing import List, Dict
+
+
+def filter_by_state(transactions: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Возвращает список словарей с указанным значением ключа state"""
-    filter_lists = []
-    for dictionary in list_state:
-        if dictionary["state"] == state:
-            filter_lists.append(dictionary)
-    return filter_lists
+    filter_transactions = []
+    for value in transactions:
+        if value["state"] == state:
+            filter_transactions.append(value)
+    return filter_transactions
 
 
-def sort_by_date(list_dict: list[dict], reduce: bool = True) -> list[dict]:
-    """Функция, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
-    (по умолчанию — убывание) и возвращает новый список, отсортированный по дате"""
-    sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=reduce)
-    return sorted_list
+def sort_by_date(transactions: List[Dict], reduce: bool = True) -> List[Dict]:
+    """
+    Функция, которая принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание) и возвращает новый список, отсортированный по дате
+    """
+    sort_transactions = sorted(transactions, key=lambda x: x["date"], reverse=reduce)
+    return sort_transactions
