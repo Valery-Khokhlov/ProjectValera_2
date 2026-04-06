c86c09535186527d684e8696734315065bf4e0e4
Valery Khokhlov <xoxlov_v83@mail.ru>
2026-04-06 20:53:11 +0300

    add test

--- src/external_api.py
+++ src/external_api.py
@@ -1,6 +1,10 @@
+from typing import Dict, Any
+
 import os
 
 import requests
+
+import random
 
 from dotenv import load_dotenv
 
@@ -12,28 +16,35 @@ load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
 
-def converting_amount_rubles(transaction: Dict) -> float:
+def converting_amount_rubles(transaction: Dict[str, Dict[str, Dict[str, Any]]]) -> float:
     """
       Принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях, тип данных — float.
      Если транзакция была в USD или EUR, происходит обращение к внешнему API для получения текущего курса валют
     и конвертации суммы операции в рубли
     """
-    for transaction in transactions_list:
 
-        code_transaction = transaction['operationAmount']['currency']['code']
+    code_transaction = transaction['operationAmount']['currency']['code']
 
-        if code_transaction == "RUB":
-            return float(transaction['operationAmount']['amount'])
+    if code_transaction == "RUB":
+        return float(transaction['operationAmount']['amount'])
 
-        elif code_transaction in ["USD", "EUR"]:
-            to = "RUB"
-            from_ = code_transaction
-            amount = transaction['operationAmount']['amount']
+    elif code_transaction in ["USD", "EUR"]:
+        to = "RUB"
+        from_= code_transaction
+        amount = transaction['operationAmount']['amount']
 
-            url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_}&amount={amount}"
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from_}&amount={amount}"
 
-            headers = {"apikey": API_KEY}
+        headers = {"apikey": API_KEY}
 
+        try:
             response = requests.get(url, headers=headers)
+            response.raise_for_status()
             result = response.json()
             return float(result['result'])
+        except requests.exceptions.HTTPError:
+            print("HTTP Error. Please check the URL.")
+            return  0.0
+        except requests.exceptions.RequestException:
+            print("An error occurred. Please try again later.")
+            return 0.0
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,6 @@
 import json
 
 
-
 def read_json_file(file_path: str) -> list:
     """
       Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях.
@@ -9,11 +8,9 @@ def read_json_file(file_path: str) -> list:
     """
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            if isinstance(data, list):
-                return data
+            transactions_list = json.load(file)
+            if isinstance(transactions_list, list):
+                return transactions_list
     except (FileNotFoundError, json.JSONDecodeError):
         pass
     return []
-
-transactions = read_json_file('data/operations.json')
